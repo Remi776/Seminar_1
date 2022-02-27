a4fbb66d5cfb3482982020a7b19e14ff60e33fd0
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-02-27 16:58:55 +0200

    add ex008

---
+++ home_work/ex_8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1) Console.WriteLine("Wrong number");
+for (int i = 1; i <= number; i++)
+{
+    if (i % 2 == 1) continue;
+    Console.Write(i + " ");
+}
