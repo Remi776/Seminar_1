2af2ea2d5d37ee2e560b8a807fcc524ab0b67c98
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-02-27 15:40:32 +0200

    add ex004

---
+++ home_work/ex_4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter number 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter number 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a > b & a > c)
+{
+    Console.WriteLine("max = " + a);
+}
+if (b > a & b > c)
+{
+    Console.WriteLine("max = " + b);
+}
+if (c > a & c > a)
+{
+    Console.WriteLine("max = " + c);
+}
+else if (a == b & b == c)
+{
+    Console.WriteLine("All numbers are equal.");
+}
