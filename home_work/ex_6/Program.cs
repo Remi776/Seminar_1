08b44d89aaeaa6e2d6cb5c967958f8a780779a6a
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-02-27 15:51:54 +0200

    add ex006

---
+++ home_work/ex_6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("An even number");    // 0 an even
+}
+else
+{
+    Console.WriteLine("An odd number");
+}
