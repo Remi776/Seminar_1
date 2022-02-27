b7477e58dabfdf33352c94374cc581390e06e45c
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-02-27 14:54:09 +0200

    add ex002

---
+++ home_work/ex_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter number 1: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter number 2: ");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("number 1 = max" + "\n" + "number 2 = min");
+}
+else
+{
+    Console.WriteLine("number 1 = min" + "\n" + "number 2 = max");
+}
