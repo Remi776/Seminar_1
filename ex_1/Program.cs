a36713586c341eb42a76de4c6a9f1ed886d58fb8
Remi776 <99408740+Remi776@users.noreply.github.com>
2022-02-27 13:54:39 +0200

    add ex001 ex003 ex 004

---
+++ ex_1/Program.cs
@@ -0,0 +1,10 @@
+﻿int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA * numberA == numberB)
+    {
+        Console.WriteLine("YES");
+    }
+else
+    {
+        Console.WriteLine("NO");
+    }
---
+++ ex_3/Program.cs
@@ -0,0 +1,30 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+    Console.WriteLine("Monday");
+}
+if (number == 2)
+{
+    Console.WriteLine("Thuesday");
+}
+if (number == 3)
+{
+    Console.WriteLine("Wed");
+}
+if (number == 4)
+{
+    Console.WriteLine("Thu");
+}
+if (number == 5)
+{
+    Console.WriteLine("Fri");
+}
+if (number == 6)
+{
+    Console.WriteLine("Sat");
+}
+if (number == 7)
+{
+    Console.WriteLine("Sun");
+}
